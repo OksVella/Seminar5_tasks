dc561e717d21d24e50d811f9d697997866d0daca
Ovella <oks.vella@yandex.ru>
2022-11-05 02:55:28 +0300

    added task 3 and scheme

---
+++ Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//Start here...
+double[] array = new double[8];
+fillArray(array);
+printArray(array);
+printDifference(array);
+
+    
+
+
+//--- Methods ---
+
+void fillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+void printArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        printFirstBracket(i);
+        Console.Write(array[i] + " ");
+        printLastBracket(i, array.Length);
+    }
+}
+
+void printFirstBracket(int index)
+{
+    if (index == 0)
+    {
+        Console.Write("[");
+    }
+}
+
+void printLastBracket(int index, int arrayLength)
+{
+    if (index == arrayLength - 1)
+    {
+        Console.Write("]");
+    }
+}
+
+void printDifference(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    Console.Write(max - min);
+}
